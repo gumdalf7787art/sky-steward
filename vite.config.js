0ff61e5569f492a0554b9b2703ace993890a5523
gumdalf7787art <gumdalf7787@gmail.com>
2026-04-23 14:10:45 +0900

    fix: replace OG thumbnail with a centered 16:9 ratio image to prevent cropping

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 2000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor': ['react', 'react-dom', 'react-router-dom', 'recoil'],
-        }
-      }
-    }
-  }
 })
