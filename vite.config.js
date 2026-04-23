0905942ce98362594ccb14b23921dbd9b501d5e9
gumdalf7787art <gumdalf7787@gmail.com>
2026-04-23 17:35:20 +0900

    perf: 빌드 안정성을 위해 번들 분할(manualChunks) 적용 및 .gitignore 정리

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom', 'recoil'],
+        }
+      }
+    }
+  }
 })
